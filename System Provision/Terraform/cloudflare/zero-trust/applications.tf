6ecc099a72fb2c92c48fa6f0505b00973c430fd8
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-03-15 16:09:17 -0400

    Policy and Application For Homelab Apps

--- System Provision/Terraform/cloudflare/zero-trust/applications.tf
+++ System Provision/Terraform/cloudflare/zero-trust/applications.tf
@@ -43,7 +43,21 @@ provider "cloudflare" {
 
 
 
+############
+# Policies #
+############
 
+resource "cloudflare_zero_trust_access_policy" "only_us_ips" {
+  account_id = var.account_id
+  decision = "allow"
+  include = [{
+    geo = {
+      country_code = "US"
+    }
+  }]
+  name = "Allow only US IPs"
+  isolation_required = false # Does not need special browser
+}
 
 
 resource "cloudflare_zero_trust_access_policy" "bypass_cloudflare_login" {
@@ -72,5 +86,38 @@ resource "cloudflare_zero_trust_access_application" "threemix-backend" {
 
   session_duration = "24h"
   skip_interstitial = true
-  tags = ["public apps"]
+  tags = ["public apps", "homelab"]
 }
+
+
+
+resource "cloudflare_zero_trust_access_application" "homelab-jellyfin" {
+    depends_on = [ cloudflare_zero_trust_access_policy.only_us_ips ]
+  domain = "jellyfin.homelab.ezequielvalencia.com"
+  type = "self_hosted"
+  account_id = var.account_id
+
+  enable_binding_cookie = true # Mitigation against CSRF, https://developers.cloudflare.com/cloudflare-one/identity/authorization-cookie/
+  http_only_cookie_attribute = true
+  name = "Jellyfin"
+
+  session_duration = "24h"
+  skip_interstitial = true
+  tags = ["homelab", "media"] # Need to create tags manually beforehand 
+}
+
+resource "cloudflare_zero_trust_access_application" "library-jellyfin" {
+    depends_on = [ cloudflare_zero_trust_access_policy.only_us_ips ]
+  domain = "library.homelab.ezequielvalencia.com"
+  type = "self_hosted"
+  account_id = var.account_id
+
+  enable_binding_cookie = true # Mitigation against CSRF, https://developers.cloudflare.com/cloudflare-one/identity/authorization-cookie/
+  http_only_cookie_attribute = true
+  name = "Library"
+
+  session_duration = "24h"
+  skip_interstitial = true
+  tags = ["homelab", "media"] # Need to create tags manually beforehand 
+}
+
