e5f375bbafe4f3490a72acdb9438fe07030feb0e
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-12-14 21:09:38 -0500

    Use Defaults When Available

--- System Provision/Terraform/proxmox/vms_zpc.tf
+++ System Provision/Terraform/proxmox/vms_zpc.tf
@@ -29,57 +29,23 @@ resource "proxmox_virtual_environment_vm" "dashy" {
     name = "Dashy"
 
     migrate                 = true
-    on_boot                 = true
-    reboot                  = false
-    started                 = true
-    reboot_after_update     = true
-
-    bios                    = "seabios"
     scsi_hardware           = "virtio-scsi-single"
-    acpi                    = true // power settings cli https://www.geeksforgeeks.org/acpi-command-in-linux-with-examples/
-    template                = false
-
-    stop_on_destroy         = false
-    tablet_device           = true
 
     protection              = true
-    keyboard_layout         = "en-us"
     tags                    = ["infra"]
     description             = local.vm_description
 
-    timeout_clone           = 1800
-    timeout_create          = 1800
-    timeout_migrate         = 1800
-    timeout_reboot          = 1800
-    timeout_shutdown_vm     = 1800
-    timeout_start_vm        = 1800
-    timeout_stop_vm         = 300
-    
-
     cpu {
         cores      = 1
-        flags      = [] 
-        hotplugged = 0
-        limit      = 0
-        numa       = false
-        sockets    = 1
         type       = "x86-64-v2-AES"
-        units      = 1024
     }
 
     disk {
-        aio               = "io_uring"
         backup            = true
-        cache             = "none"
-        datastore_id      = "local-lvm"
-        discard           = "ignore"
-        file_format       = "raw"
         interface         = "scsi0"
         iothread          = true
         path_in_datastore = "vm-301-disk-0"
-        replicate         = true
         size              = 11
-        ssd               = false
     }
 
     initialization {
@@ -94,22 +60,11 @@ resource "proxmox_virtual_environment_vm" "dashy" {
 
     memory {
         dedicated      = 2048
-        floating       = 0
-        keep_hugepages = false
-        shared         = 0
     }
 
     network_device {
-        bridge       = "vmbr0"
-        disconnected = false
-        enabled      = true
         firewall     = true
         mac_address  = "BC:24:11:9B:56:AB"
-        model        = "virtio"
-        mtu          = 0
-        queues       = 0
-        rate_limit   = 0
-        vlan_id      = 0
     }
 
     operating_system {
@@ -123,57 +78,23 @@ resource "proxmox_virtual_environment_vm" "pihole" {
     name = "Pihole"
 
     migrate                 = true
-    on_boot                 = true
-    reboot                  = false
-    started                 = true
-    reboot_after_update     = true
-
-    bios                    = "seabios"
     scsi_hardware           = "virtio-scsi-single"
-    acpi                    = true // power settings cli https://www.geeksforgeeks.org/acpi-command-in-linux-with-examples/
-    template                = false
-
-    stop_on_destroy         = false
-    tablet_device           = true
 
     protection              = true
-    keyboard_layout         = "en-us"
     tags                    = ["infra"]
     description             = "Set the router DNS to this IP address and it should work on all devices. For some reason with the current router it doesn't but that's how it goes I guess."
-
-    timeout_clone           = 1800
-    timeout_create          = 1800
-    timeout_migrate         = 1800
-    timeout_reboot          = 1800
-    timeout_shutdown_vm     = 1800
-    timeout_start_vm        = 1800
-    timeout_stop_vm         = 300
     
-
     cpu {
         cores      = 1
-        flags      = [] 
-        hotplugged = 0
-        limit      = 0
-        numa       = false
-        sockets    = 1
         type       = "x86-64-v2-AES"
-        units      = 1024
     }
 
     disk {
-        aio               = "io_uring"
         backup            = true
-        cache             = "none"
-        datastore_id      = "local-lvm"
-        discard           = "ignore"
-        file_format       = "raw"
         interface         = "scsi0"
         iothread          = true
         path_in_datastore = "vm-300-disk-0"
-        replicate         = true
         size              = 10
-        ssd               = false
     }
 
     initialization {
@@ -188,22 +109,11 @@ resource "proxmox_virtual_environment_vm" "pihole" {
 
     memory {
         dedicated      = 2048
-        floating       = 0
-        keep_hugepages = false
-        shared         = 0
     }
 
     network_device {
-        bridge       = "vmbr0"
-        disconnected = false
-        enabled      = true
         firewall     = true
         mac_address  = "BC:24:11:06:21:9E"
-        model        = "virtio"
-        mtu          = 0
-        queues       = 0
-        rate_limit   = 0
-        vlan_id      = 0
     }
 
     operating_system {
@@ -217,76 +127,32 @@ resource "proxmox_virtual_environment_vm" "monitor" {
     name = "Monitor"
 
     migrate                 = true
-    on_boot                 = true
-    reboot                  = false
-    started                 = true
-    reboot_after_update     = true
-
-    bios                    = "seabios"
     scsi_hardware           = "virtio-scsi-single"
-    acpi                    = true // power settings cli https://www.geeksforgeeks.org/acpi-command-in-linux-with-examples/
-    template                = false
-
-    stop_on_destroy         = false
-    tablet_device           = true
 
     protection              = true
-    keyboard_layout         = "en-us"
     tags                    = ["kubernetes"]
-
-    timeout_clone           = 1800
-    timeout_create          = 1800
-    timeout_migrate         = 1800
-    timeout_reboot          = 1800
-    timeout_shutdown_vm     = 1800
-    timeout_start_vm        = 1800
-    timeout_stop_vm         = 300
-    
 
     cpu {
         cores      = 3
-        flags      = [] 
-        hotplugged = 0
-        limit      = 0
-        numa       = false
-        sockets    = 1
         type       = "x86-64-v2-AES"
-        units      = 1024
     }
 
     disk {
-        aio               = "io_uring"
         backup            = true
-        cache             = "none"
-        datastore_id      = "hdd"
-        discard           = "ignore"
-        file_format       = "raw"
         interface         = "scsi0"
+        datastore_id      = "hdd"
         iothread          = true
         path_in_datastore = "vm-302-disk-0"
-        replicate         = true
         size              = 32
-        ssd               = false
     }
 
     memory {
         dedicated      = 10240
-        floating       = 0
-        keep_hugepages = false
-        shared         = 0
     }
 
     network_device {
-        bridge       = "vmbr0"
-        disconnected = false
-        enabled      = true
         firewall     = true
         mac_address  = "BC:24:11:4F:7F:B2"
-        model        = "virtio"
-        mtu          = 0
-        queues       = 0
-        rate_limit   = 0
-        vlan_id      = 0
     }
 
     operating_system {
@@ -300,76 +166,32 @@ resource "proxmox_virtual_environment_vm" "longhorn" {
     name = "ZPC-Longhorn"
 
     migrate                 = true
-    on_boot                 = true
-    reboot                  = false
-    started                 = true
-    reboot_after_update     = true
-
-    bios                    = "seabios"
     scsi_hardware           = "virtio-scsi-single"
-    acpi                    = true // power settings cli https://www.geeksforgeeks.org/acpi-command-in-linux-with-examples/
-    template                = false
-
-    stop_on_destroy         = false
-    tablet_device           = true
 
     protection              = true
-    keyboard_layout         = "en-us"
     tags                    = ["kubernetes"]
-
-    timeout_clone           = 1800
-    timeout_create          = 1800
-    timeout_migrate         = 1800
-    timeout_reboot          = 1800
-    timeout_shutdown_vm     = 1800
-    timeout_start_vm        = 1800
-    timeout_stop_vm         = 300
-    
 
     cpu {
         cores      = 2
-        flags      = [] 
-        hotplugged = 0
-        limit      = 0
-        numa       = false
-        sockets    = 1
         type       = "x86-64-v2-AES"
-        units      = 1024
     }
 
     disk {
-        aio               = "io_uring"
         backup            = true
-        cache             = "none"
-        datastore_id      = "hdd"
-        discard           = "ignore"
-        file_format       = "raw"
         interface         = "scsi0"
+        datastore_id      = "hdd"
         iothread          = true
         path_in_datastore = "vm-500-disk-0"
-        replicate         = true
         size              = 42
-        ssd               = false
     }
 
     memory {
         dedicated      = 4096
-        floating       = 0
-        keep_hugepages = false
-        shared         = 0
     }
 
     network_device {
-        bridge       = "vmbr0"
-        disconnected = false
-        enabled      = true
         firewall     = true
         mac_address  = "BC:24:11:4F:FE:C3"
-        model        = "virtio"
-        mtu          = 0
-        queues       = 0
-        rate_limit   = 0
-        vlan_id      = 0
     }
 
     operating_system {
