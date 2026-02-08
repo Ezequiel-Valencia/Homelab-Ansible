33de42ec200e3726da9d9f8cba08460a65c990a1
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-02-08 15:15:48 -0500

    Add Remote Backups

--- System Provision/Bash Scripts/backup.py
+++ System Provision/Bash Scripts/backup.py
@@ -1,9 +1,11 @@
 import subprocess
 import os
 import time
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 import py7zr
 from dataclasses import dataclass
+import boto3
+import json
 
 @dataclass(frozen=True)
 class BackupSpec:
@@ -23,7 +25,7 @@ BACKUPS = [
     BackupSpec(dir_path="/volume1/k8data/storage/mobilizon", remote_backup=False, aws_key="")
 ]
 
-ENCRYPT_PASSWORD=""
+ENV_FILE_PATH = ""
 
 # Destination directory for rsync and zips
 DEST_DIR = "/volume1/@home/ezequiel/backups"
@@ -32,6 +34,11 @@ all_zips_folder = f"{DEST_DIR}/zips"
 
 
 #!-------------------------------------------!#
+
+ENCRYPT_PASSWORD=""
+AWS_ACCESS_KEY=""
+AWS_SECRET_KEY=""
+BUCKET_NAME=""
 
 def run_rsync(src: str, dest: str) -> None:
     """Run rsync to sync a directory to the destination."""
@@ -67,8 +74,38 @@ def cleanup_old_backups(backup_path: str, max_age_days=14) -> None:
                 os.remove(file_path)
 
 
+def should_upload_happen(s3_client, backup: BackupSpec) -> tuple[bool, str]:
+    try:
+        two_weeks_ago = datetime.now(timezone.utc) - timedelta(weeks=2)
+        response = s3_client.head_object(Bucket=BUCKET_NAME, Key=backup.aws_key)
+        last_modified = response['LastModified']
+        
+        if last_modified < two_weeks_ago:
+            print(f"Old file found (Modified: {last_modified}). Deleting...")
+            s3_client.delete_object(Bucket=BUCKET_NAME, Key=backup.aws_key)
+            return True, "Last upload is over two weeks old."
+        else:
+            return False, "Current archive is less than 2 weeks old. Skipping upload."
+    except s3_client.exceptions.ClientError as e:
+        if e.response["ResponseMetadata"]["HTTPStatusCode"] == 404:
+            return True, "No existing zip file found in bucket. Proceeding..."
+
+
+def remote_backup(backup: BackupSpec, archive_file_path: str):
+    s3 = boto3.client('s3', region_name="us-east-1", aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+    try:
+        should_upload, reason = should_upload_happen(s3_client=s3, backup=backup)
+        print(reason)
+        if should_upload:
+            print(f"Uploading {archive_file_path} to S3 bucket {BUCKET_NAME}...")
+            s3.upload_file(archive_file_path, BUCKET_NAME, backup.aws_key)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def main():
     for backup in BACKUPS:
+        print("=======================================")
         src = backup.dir_path
         folder_name: str = os.path.basename(os.path.normpath(src))
         apps_cache: str = f"{rsync_folder_cache}/{folder_name}"
@@ -79,11 +116,23 @@ def main():
 
         print(f"Zipping {apps_cache}")
         zip_file = zip_directory(src_dir=apps_cache, output_dir=zip_path)
-        print(f"Created: {zip_file}\n")
+        print(f"Created: {zip_file}")
 
         print("Checking for old backups...")
         cleanup_old_backups(backup_path=zip_path)
+        
+        if backup.remote_backup:
+            print("\n******************************")
+            print(f"Starting Remote Backup for {zip_file} as key {backup.aws_key}")
+            remote_backup(backup=backup, archive_file_path=zip_file)
 
+        print("\n=======================================\n")
 
 if __name__ == "__main__":
+    with open(ENV_FILE_PATH, "r") as env:
+        sec = json.load(env)
+        BUCKET_NAME = sec["bucket_name"]
+        AWS_ACCESS_KEY = sec["aws_access_key"]
+        AWS_SECRET_KEY = sec["aws_secret_key"]
+        ENCRYPT_PASSWORD = sec["encrypt_password"]
     main()
