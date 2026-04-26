b48aa6b450f898f79bf15d98c182e2ea86e0fa02
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-04-26 14:33:08 -0400

    Update LDAP Client Config

--- roles/setup/ldap-client/debug/list_all.sh
+++ roles/setup/ldap-client/debug/list_all.sh
@@ -1,1 +1,10 @@
-ldapsearch -H ldaps://ldap.homelab:636 -b "dc=homelab,dc=cool" -x -D "uid=binding_user,ou=people,dc=homelab,dc=cool" -W
+ldapsearch -H ldaps://ldap.homelab:636 \
+-b "ou=users,dc=ldap,dc=homelab,dc=cool" \
+-x -D "cn=ldapservice,ou=users,dc=ldap,dc=homelab,dc=cool" -W \
+'(memberOf=cn=linux-user,ou=groups,dc=ldap,dc=homelab,dc=cool)' | less
+
+# The (memberOf) is a filter, and ensures the returned set of users is not random service 
+# accounts or app accounts, that should not become linux users. Aka jellyfin-theater should 
+# only be an app account, not Linux and it'll be reflected by not being in the 'linux-user'
+# group which is currently being filtered on.
+
