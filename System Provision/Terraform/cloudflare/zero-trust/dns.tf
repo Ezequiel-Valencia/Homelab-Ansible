94136957328ded19bc6f798e38b9e2b01d744d33
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-05-02 11:14:13 -0400

    DNS Via Terraform

---
+++ System Provision/Terraform/cloudflare/zero-trust/dns.tf
@@ -0,0 +1,152 @@
+
+locals {
+  tunnel_cname = "${cloudflare_zero_trust_tunnel_cloudflared.homelab_tunnel.id}.cfargotunnel.com"
+}
+
+##########################
+# Homelab Tunnel Records #
+##########################
+
+resource "cloudflare_dns_record" "jellyfin" {
+  zone_id = var.zone_id
+  name    = "jellyfin.homelab.tunnel.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "library" {
+  zone_id = var.zone_id
+  name    = "library.homelab.tunnel.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "karakeep" {
+  zone_id = var.zone_id
+  name    = "karakeep.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "ittools" {
+  zone_id = var.zone_id
+  name    = "ittools.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "threemix-backend" {
+  zone_id = var.zone_id
+  name    = "backend.threemix.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "backend" {
+  zone_id = var.zone_id
+  name    = "backend.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "openweb-ui" {
+  zone_id = var.zone_id
+  name    = "openweb-ui.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "n8n" {
+  zone_id = var.zone_id
+  name    = "n8n.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "dashy" {
+  zone_id = var.zone_id
+  name    = "dashy.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "gitea" {
+  zone_id = var.zone_id
+  name    = "gitea.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "openproject" {
+  zone_id = var.zone_id
+  name    = "openproject.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "leantime" {
+  zone_id = var.zone_id
+  name    = "leantime.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "home-assistant" {
+  zone_id = var.zone_id
+  name    = "home-assistant.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "joplin" {
+  zone_id = var.zone_id
+  name    = "joplin.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "authentik" {
+  zone_id = var.zone_id
+  name    = "authentik.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "mealie" {
+  zone_id = var.zone_id
+  name    = "mealie.tunnel.homelab.ezequielvalencia.com"
+  type    = "CNAME"
+  content = local.tunnel_cname
+  proxied = true
+  ttl     = 1
+}
--- System Provision/Terraform/cloudflare/zero-trust/imports.tf
+++ System Provision/Terraform/cloudflare/zero-trust/imports.tf
@@ -14,4 +14,92 @@
 # }
 
 
+################
+# DNS Records  #
+# ID format: ${var.zone_id}/<dns_record_id>
+# DNS record IDs can be found via: Cloudflare Dashboard > DNS > Records, or via API/CLI
+# Look up each DNS record ID from the Cloudflare dashboard (DNS > Records) or via the API:
+#   curl -X GET "https://api.cloudflare.com/client/v4/zones/<zone_id>/dns_records" \
+#     -H "Authorization: Bearer <token>"
+################
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.jellyfin
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.library
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.karakeep
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.ittools
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.threemix-backend
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.backend
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.openweb-ui
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.n8n
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.dashy
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.gitea
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.openproject
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.leantime
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.home-assistant
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.joplin
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.authentik
+# }
+
+# import {
+#   id = "${var.zone_id}/<dns_id>"
+#   to = cloudflare_dns_record.mealie
+# }
 
